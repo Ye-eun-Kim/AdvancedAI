b4f1b9f493df71b4ac8646034153175bc0f775f0
Ye-eun-Kim <kimyeeun@gm.gist.ac.kr>
2023-11-28 01:00:15 +0000

    modify codes to fix seed draw and savd graphs do experiments

--- 01_basic.py
+++ 01_basic.py
@@ -10,24 +10,53 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.preprocessing.image import img_to_array, array_to_img
 import cv2
 import tensorflow_hub as hub
+import matplotlib.pyplot as plt
+import random
 
 
+
+# Fix a seed to make result always the same
+SEED = 123
+tf.random.set_seed(SEED)
+np.random.seed(SEED)
+random.seed(SEED)
+
+
+
+# Define a function to apply gray scaling to images
+def random_grayscale(image, p=0.5):
+    """Apply grayscaling to the image with probability p."""
+    if np.random.rand() < p:
+        gray_image = tf.image.rgb_to_grayscale(image)
+        # plt.imshow(gray_image/255.0)
+        rgb_gray_image = tf.image.grayscale_to_rgb(gray_image)
+        # plt.imshow(rgb_gray_image/255.0)
+        return rgb_gray_image 
+
+    return image
 
 # Define a function to apply histogram equalization to images
 def histogram_equalization(image):
     # Convert to array
     img_array = img_to_array(image)
-
     # Convert to YUV color space
     yuv_img_array = cv2.cvtColor(img_array, cv2.COLOR_RGB2YUV)
-
     # Apply histogram equalization to the Y channel
     yuv_img_array[:, :, 0] = cv2.equalizeHist(yuv_img_array[:, :, 0].astype('uint8'))
-
     # Convert back to RGB color space
     equalized_img_array = cv2.cvtColor(yuv_img_array, cv2.COLOR_YUV2RGB)
 
     return equalized_img_array
+
+# Define a function to apply preprocessing functions
+def custom_preprocessing_function(image):
+    # Apply histogram equalization or other preprocessing steps first
+    image = histogram_equalization(image)
+    # Then apply random grayscaling
+    image = random_grayscale(image)
+    return image
+
+
 
 # Define models
 # Define a common CNN model
@@ -58,7 +87,7 @@ def create_resnet50_transfer_model(input_shape, num_classes):
     return model
 
 # Define the ResNet18 model for transfer learning
-def create_resnet_hub_model(input_shape, num_classes):
+def create_resnet18_transfer_model(input_shape, num_classes):
     # Load ResNet model from TensorFlow Hub
     resnet_hub_url = "https://tfhub.dev/google/imagenet/resnet_v2_50/feature_vector/4"  # Example URL for ResNet50
     resnet_hub_model = hub.KerasLayer(resnet_hub_url, input_shape=input_shape, trainable=False)
@@ -69,6 +98,7 @@ def create_resnet_hub_model(input_shape, num_classes):
         Dense(num_classes, activation='softmax')
     ])
     return model
+
 
 
 # Define a function to run experiments varifying batch size and model type
@@ -89,16 +119,17 @@ def run_experiment(batch_size, model_type, augmentation_flag):
     # Create data generators
     if augmentation_flag == 1:
         datagen = ImageDataGenerator(
-            rescale=1./255,
-            preprocessing_function=histogram_equalization,
-            rotation_range=40,
-            width_shift_range=0.2,
-            height_shift_range=0.2,
-            shear_range=0.2,
-            horizontal_flip=True,
-            fill_mode='nearest',
-            validation_split=0.2
-        )
+        rescale=1./255,
+        preprocessing_function=custom_preprocessing_function,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest',
+        validation_split=0.2
+    )
     else:
         datagen = ImageDataGenerator(
             rescale=1./255,
@@ -149,9 +180,13 @@ def run_experiment(batch_size, model_type, augmentation_flag):
     elif model_type == 'resnet18':
         model = create_resnet18_transfer_model(input_shape=input_shape, num_classes=4)
 
+    # To deal with the problem in training of something with augmentation
+    if aug_flags == 0:
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    else:
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.0009)
     # Compile the model
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    # sparsecategoricalcrossentropy
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
     # Define early stopping criteria
     early_stopping = EarlyStopping(
@@ -160,49 +195,66 @@ def run_experiment(batch_size, model_type, augmentation_flag):
         restore_best_weights=True
     )
     
+    
     # Train the model
     history = model.fit(
         train_generator,
-        steps_per_epoch=math.ceil(len(train_generator)),
+        steps_per_epoch=math.ceil(len(train_generator)),  # This makes error in testing, so I delete it(training with default)
         epochs=NUM_EPOCHS,
         validation_data=test_generator,
-        validation_steps=math.ceil(len(train_generator)),
+        validation_steps=math.ceil(len(test_generator)),
         callbacks=[early_stopping]
     )
 
 
-    # Evaluate the model
+    # Create subplots
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+    # Plot training & validation accuracy values
+    ax1.plot(history.history['accuracy'], label='Train')
+    ax1.plot(history.history['val_accuracy'], label='Test')
+    ax1.set_title(f'Model Accuracy ({model_type}, {batch_size}, {augmentation_flag})')
+    ax1.set_ylabel('Accuracy')
+    ax1.set_xlabel('Epoch')
+    ax1.legend(loc='upper left')
+
+    # Plot training & validation loss values
+    ax2.plot(history.history['loss'], label='Train')
+    ax2.plot(history.history['val_loss'], label='Test')
+    ax2.set_title('Model Loss')
+    ax2.set_ylabel('Loss')
+    ax2.set_xlabel('Epoch')
+    ax2.legend(loc='upper left')
+
     val_loss, val_accuracy = model.evaluate(val_generator, steps=len(val_generator))
-
-    ## Generate predictions and calculate cm and cr
-    # val_predictions = model.predict(val_generator, steps=len(val_generator))
-    # predicted_classes = np.argmax(val_predictions, axis=1)
-    # true_classes = val_generator.classes
-    # cm = confusion_matrix(true_classes, predicted_classes)
-    # cr = classification_report(true_classes, predicted_classes)
-
-    # Save results to file
-    output_file_name = f'results/multi_results.txt'
-    with open(output_file_name, 'a') as file:
-        # file.write(f'Validation Loss: {val_loss}\n')
-        file.write(f'{model_type:10}, {batch_size:3}, {augmentation_flag}: {val_accuracy}\n\n')
-        # file.write("Confusion Matrix:\n")
-        # cm_str = '\n'.join(['\t'.join([str(cell) for cell in row]) for row in cm])
-        # file.write(cm_str + "\n\n")
-        # file.write("Classification Report:\n")
-        # file.write(cr)
+    print(f'val_loss: {val_loss}, val_accuracy: {val_accuracy}')
+    
+    # Add text with validation results to the bottom of the figure
+    fig.text(0.5, 0.01, f'Validation Accuracy: {val_accuracy:.4f},      Validation Loss: {val_loss:.4f}', 
+             ha='center', va='center', fontsize=12)
+    
+    # Save the plot to a file and show the plot
+    plt.tight_layout()
+    file_save_path = 'Graphs/03/'
+    if os.path.exists(file_save_path):
+        pass
+    else:
+        os.mkdir(file_save_path)
+    plt.savefig(file_save_path+f'{model_type}_{batch_size}_{augmentation_flag}.png')
 
 # Running experiments
-batch_sizes = [4]
+batch_sizes = [4, 8, 16, 32]
 model_types = ['common', 'resnet50', 'resnet18']
 # model_types = ['common']
 aug_flags = [0, 1]
 
 
-for model_type in model_types:
-    for batch_size in batch_sizes:
-        for augmentation_flag in aug_flags:
-            run_experiment(batch_size, model_type, augmentation_flag)
+# # for model_type in model_types:
+# for batch_size in batch_sizes:
+#     for augmentation_flag in aug_flags:
+#         run_experiment(batch_size, 'common', augmentation_flag)
+run_experiment(8, 'common', 1)
+run_experiment(16, 'common', 1)
 
 
 # https://stackoverflow.com/questions/63636565/datagen-flow-from-directory-function
